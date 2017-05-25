9724a2ad63f71029fd26875686e0be94478cd51e
Roman Levitas <rlevitas@rbmtechnologies.com>
2017-05-25 15:21:10 -0400

    display questions, ids, answers

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import QuestionTable from './QuestionTable';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return(
-      <div></div>
+      <div>
+        <h1>Launch Academy FAQ</h1>
+        <QuestionTable questions={this.props.data} />
+      </div>
     )
   }
 }

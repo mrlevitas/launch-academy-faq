2f7ffd26643e86e7c0f350d4a7aabd83579a7c5d
Roman Levitas <levitas1@illinois.edu>
2017-05-28 19:22:48 -0400

    toggle question

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,12 +3,33 @@ import React from 'react';
 class Question extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+    this.state = { isAnswerDisplayed: false };
+  }
+
+  handleToggleClick() {
+    this.setState({ isAnswerDisplayed: !this.state.isAnswerDisplayed });
   }
 
   render() {
+    const isAnswerDisplayed = this.state.isAnswerDisplayed;
+
+    let answer = null;
+    let button = null;
+
+    if (isAnswerDisplayed) {
+      answer = <div>{this.props.data.answer}</div>
+      button = <button onClick={this.handleToggleClick} color="#3d9648">-</button>
+    } else {
+      button = <button onClick={this.handleToggleClick} color="#444547">+</button>
+    }
+
     return (
-        <div id={this.props.data.id}>{this.props.data.question}
-          <div>{this.props.data.answer}</div>
+        <div id={this.props.data.id}>
+          {button}
+          <h2>{this.props.data.question}</h2>
+          {answer}
         </div>
 
     );

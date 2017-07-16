67fc4d38fa575281370275b1778040685fa4a445
Roman Levitas <levitas1@illinois.edu>
2017-07-16 19:35:27 -0400

    there can only be One

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,32 +3,33 @@ import React from 'react';
 class Question extends React.Component {
   constructor(props) {
     super(props);
-
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-    this.state = { isAnswerDisplayed: false };
-  }
-
-  handleToggleClick() {
-    this.setState({ isAnswerDisplayed: !this.state.isAnswerDisplayed });
   }
 
   render() {
-    const isAnswerDisplayed = this.state.isAnswerDisplayed;
+    const isAnswerDisplayed = this.props.isAnswerDisplayed;
 
     let answer = null;
-    let button = null;
+    let button_class = "fa fa-plus-square";
+
+    let buttonClick = () =>
+     this.props.onClickFunction(this.props.data.id);
 
     if (isAnswerDisplayed) {
       answer = <div>{this.props.data.answer}</div>
-      button = <button onClick={this.handleToggleClick} color="#3d9648">-</button>
-    } else {
-      button = <button onClick={this.handleToggleClick} color="#444547">+</button>
+      button_class = "fa fa-minus-square"
     }
 
+    let button = <i onClick={buttonClick} className={button_class}/>
+
     return (
-        <div id={this.props.data.id}>
-          {button}
-          <h2>{this.props.data.question}</h2>
+        <div id={this.props.data.id} className="faq">
+          <div className="button">
+            {button}
+          </div>
+
+          <div className="question">
+            <h2>{this.props.data.question}</h2>
+          </div>
           {answer}
         </div>
 
